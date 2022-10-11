2b9a104dd6b464734d587a7b142f47f2e333314c
fotini <f.batzoni@gmail.com>
2022-10-11 14:45:32 +0300

    Optimasations

--- src/App.js
+++ src/App.js
@@ -31,11 +31,20 @@ class App extends Component {
       ))
   }
 
+  onSearchChange = (event) =>{
+    const searchField = event.target.value.toLocaleLowerCase();
+   this.setState(() =>{
+    return {searchField}
+   })
+  }
+
   render(){
     console.log('render')
 
-    const filteredMonsters = this.state.monsters.filter(monster=>{ 
-      return  monster.name.toLowerCase().includes(this.state.searchField)
+    const {monsters, searchField} = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter(monster=>{ 
+      return  monster.name.toLowerCase().includes(searchField)
     });
 
 
@@ -45,12 +54,7 @@ class App extends Component {
           className='search-box' 
           type='search' 
           placeholder= 'search monsters' 
-          onChange={(event) =>{
-            const searchField = event.target.value.toLocaleLowerCase();
-           this.setState(() =>{
-            return {searchField}
-           })
-        }}/>
+          onChange={onSearchChange}/>
         {
           filteredMonsters.map((monster)=>{
            return <div key={monster.id}><h1>{monster.name}</h1></div>
